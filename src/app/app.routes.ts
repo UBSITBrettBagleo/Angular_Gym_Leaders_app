8f88e4fd086f38758e38636feb6c9288ed2683be
bagleo_b <bagleo_b@f112.ub.lan>
2026-06-11 10:53:52 +0800

    added Hoenn route

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,10 +2,12 @@ import { Routes } from '@angular/router';
 import { Home } from './home/home';
 import { Kanto } from './kanto/kanto';
 import { Johto } from './johto/johto';
+import { Hoenn} from './hoenn/hoenn';
 
 export const routes: Routes = [
   { path: '', redirectTo: 'home', pathMatch: 'full' },
   { path: 'home', component: Home },
   { path: 'kanto', component: Kanto },
-  { path: 'johto', component: Johto }
+  { path: 'johto', component: Johto },
+  { path: 'hoenn', component: Hoenn}
 ];
