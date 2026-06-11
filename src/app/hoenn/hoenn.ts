c849ea88893ead16c04b81de137635d630d1aef7
bagleo_b <bagleo_b@f112.ub.lan>
2026-06-11 10:50:31 +0800

    Connect Hoenn component to service

--- src/app/hoenn/hoenn.ts
+++ src/app/hoenn/hoenn.ts
@@ -1,9 +1,15 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { HoennService } from '../hoenn-service/hoenn-service';
 
 @Component({
   selector: 'app-hoenn',
+  standalone: true;
   imports: [],
   templateUrl: './hoenn.html',
   styleUrl: './hoenn.css',
 })
-export class Hoenn {}
+export class Hoenn {
+
+  hoennService = inject(HoennService);
+
+}
